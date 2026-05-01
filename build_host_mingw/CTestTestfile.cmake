feae4f6f026851b895f1af5231f8d81f457a7306
FARUmihu <335506628@qq.com>
2026-05-01 11:56:58 +0800

    fix：增添了兼容windows也可以直接运行仿真的虚拟环境

---
+++ build_host_mingw/CTestTestfile.cmake
@@ -0,0 +1,28 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/FMI/Documents/GitHub/FengMH/App/test
+# Build directory: C:/Users/FMI/Documents/GitHub/FengMH/build_host_mingw
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_pid "C:/Users/FMI/Documents/GitHub/FengMH/build_host_mingw/test_pid.exe")
+set_tests_properties(test_pid PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;84;add_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;87;add_app_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;0;")
+add_test(test_proto_frame "C:/Users/FMI/Documents/GitHub/FengMH/build_host_mingw/test_proto_frame.exe")
+set_tests_properties(test_proto_frame PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;84;add_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;88;add_app_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;0;")
+add_test(test_motor_registry "C:/Users/FMI/Documents/GitHub/FengMH/build_host_mingw/test_motor_registry.exe")
+set_tests_properties(test_motor_registry PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;84;add_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;89;add_app_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;0;")
+add_test(test_log "C:/Users/FMI/Documents/GitHub/FengMH/build_host_mingw/test_log.exe")
+set_tests_properties(test_log PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;84;add_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;90;add_app_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;0;")
+add_test(test_bsp_fdcan "C:/Users/FMI/Documents/GitHub/FengMH/build_host_mingw/test_bsp_fdcan.exe")
+set_tests_properties(test_bsp_fdcan PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;84;add_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;91;add_app_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;0;")
+add_test(test_gait "C:/Users/FMI/Documents/GitHub/FengMH/build_host_mingw/test_gait.exe")
+set_tests_properties(test_gait PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;84;add_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;92;add_app_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;0;")
+add_test(test_task_comm "C:/Users/FMI/Documents/GitHub/FengMH/build_host_mingw/test_task_comm.exe")
+set_tests_properties(test_task_comm PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;84;add_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;93;add_app_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;0;")
+add_test(test_leg_controller "C:/Users/FMI/Documents/GitHub/FengMH/build_host_mingw/test_leg_controller.exe")
+set_tests_properties(test_leg_controller PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;84;add_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;94;add_app_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;0;")
+add_test(test_script "C:/Users/FMI/Documents/GitHub/FengMH/build_host_mingw/test_script.exe")
+set_tests_properties(test_script PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;84;add_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;95;add_app_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;0;")
+add_test(test_task_chassis "C:/Users/FMI/Documents/GitHub/FengMH/build_host_mingw/test_task_chassis.exe")
+set_tests_properties(test_task_chassis PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;84;add_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;96;add_app_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;0;")
+add_test(test_host_sim "C:/Users/FMI/Documents/GitHub/FengMH/build_host_mingw/test_host_sim.exe")
+set_tests_properties(test_host_sim PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;84;add_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;97;add_app_test;C:/Users/FMI/Documents/GitHub/FengMH/App/test/CMakeLists.txt;0;")
